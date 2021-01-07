227ffcb4c9bc4898c060b5dc2e2c9910cc18e7ba
alemprogramer <alemhhaisb@gmail.com>
2021-01-07 19:58:03 +0600

    update active manue

--- language.js
+++ language.js
@@ -1,28 +1,28 @@
 function showit(myDIV) {
     if (myDIV.style.display === "none") {
-      myDIV.style.display = "block";
+        myDIV.style.display = "block";
     } else {
-      myDIV.style.display = "none";
+        myDIV.style.display = "none";
     }
-  }
+}
 
-  function show() {
+function show() {
     var myDIV = document.getElementById("mycls");
     showit(myDIV);
-  }
+}
 
-  show();
-    setTimeout(show, 25000);
+show();
+setTimeout(show, 25000);
 
- var header = document.getElementById("myDIV");
-var btns = document.getElementsByClassName("btn");
+var header = document.getElementById("mycls");
+var btns = document.getElementsByClassName("text-color");
 for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function() {
-  var current = document.getElementsByClassName("active");
-  current[0].className = current[0].className.replace(" active", "");
-  this.className += " active";
-  });
+    btns[i].addEventListener("click", function () {
+        var current = document.getElementsByClassName("active");
+        current[0].className = current[0].className.replace(" active", "");
+        this.className += " active";
+    });
 }
 
 
-  console.log('woking');
+console.log('woking');
